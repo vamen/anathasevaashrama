d41588f6f255da5d49e64fac69817f2d99a8b49a
vivekbala <vivekbalachandra@gmail.com>
2018-10-04 21:55:59 +0530

    ajax login added

--- cuserauth/static/cuserauth/script/login.js
+++ cuserauth/static/cuserauth/script/login.js
@@ -1,8 +1,63 @@
-function validate(){
-    var user=document.getElementById("username")
-    var sel = document.getElementById("colLister")
-    var pass = document.getElementById("password")
+$(document).ready(function(){
+
+    // jQuery methods go here...
+
+    alert("alert")
+    $("#login-submit").click(function(){
+            username=$("#username").val()
+            college=$("#coll_drop_down option:selected").text()         
+            password=$("#password").val()
+            alert(username+":::"+college)
+            $.ajax({
+                type: "POST",
+                url: "/login",
+                data: JSON.stringify({"username":username,"college":college,"password":password}),
+                success: function(msg){
+                    console.log("data "+response)
+                    if(msg.status == "success"){
+                        alert("redirecting page ...")
+                    }
+                },
+                error:function(msg){
+                    console.log("response error")
+                },
+                dataType: "json"
+              });
+
+        });
+
+
     
-    alert("Enter Valid data"+sel.value)
-    return true
-}
+ });
+
+// function validate_and_submit()
+// {
+    
+   
+//     var user = document.getElementById("username")
+//     var sel = document.getElementById("colLister")
+//     var pass = document.getElementById("password")
+    
+//     username=user.value 
+//     college=$("#dropdownList option:selected").text()
+//     password=pass.value
+    
+//     alert("Enter Valid data"+college)
+//     alert(username+password+college)
+//     //validation goes here
+    
+//     $.ajax({
+//         type: "POST",
+//         url: "/login",
+//         data: {"username":user,"college":sel},
+//         success: function(msg){
+//             console.log("data "+response)
+//         },
+//         error:function(msg){
+//             console.log("response error")
+//         },
+//         dataType: "json"
+//       });
+    
+//     return false
+// }
