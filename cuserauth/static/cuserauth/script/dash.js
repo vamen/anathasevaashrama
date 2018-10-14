650f625b22600f18cb39c4b092e8ed077684b002
vivekbala <vivekbalachandra@gmail.com>
2018-10-14 17:28:42 +0530

    row click for attendance added

--- cuserauth/static/cuserauth/script/dash.js
+++ cuserauth/static/cuserauth/script/dash.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+    
     window.history.forward();
-    $('#section-table').DataTable();
+    
+    var marks_table=$('#marks-table').DataTable();
+
+    var attendance_table=$('#attendance-table').DataTable();
+
+    $('#attendance-table tbody').on('click', 'tr', function() {
+        console.log('API row values : ', attendance_table.row(this).data());
+        subCode=attendance_table.row(this).data()["DT_RowId"]
+        
+        college_code=Cookies.get('collegeCode');
+        userid=Cookies.get('userid');
+        // token=Cookies.get("token")
+       
+        $.ajax({
+            type: "GET",
+            url: "/sectionHandler",
+
+            data: JSON.stringify({"collegeCode":college_code,"userID":userid,"subCode":subCode}),
+            success: function(msg){
+                
+                   console.log(msg) 
+            },
+            error:function(msg){
+                console.log("response error")
+            },
+            dataType: "json"
+          });
+    })
+
+
 })
